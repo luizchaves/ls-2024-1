e9889942e802524f8674bbe78ccec45e43e8b50c
Luiz <lucachaves@gmail.com>
2024-03-06 15:41:44 -0300

    sintaxe de array

---
+++ classnotes/ecma/array.js
@@ -0,0 +1,98 @@
+// create array with values
+let values = [1, 2, 3, 4, 5];
+
+// access the array elements
+console.log(values); //=> [1, 2, 3, 4, 5]
+console.log(values[0]); //=> 1
+console.log(values.at(-1)); //=> 5
+
+// create empty array
+values = [];
+
+// adding
+values[0] = 1;
+console.log(values); //=> [1]
+
+values[1] = 2;
+console.log(values); //=> [1, 2]
+
+values.push(3);
+console.log(values); //=> [1, 2, 3]
+
+// changing values
+values[0] = 10;
+
+// mutable [0, 1, 2]
+const numbers = [0, 1, 2];
+// numbers = 10; // TypeError: Assignment to constant variable.
+
+// [0, 1, 2] <= 3
+numbers.push(3);
+console.log(numbers); //=> [0, 1, 2, 3]
+
+// -1 => [0, 1, 2, 3]
+numbers.unshift(-1);
+console.log(numbers); //=> [-1, 0, 1, 2, 3]
+
+// multiple types
+const person = ['Alice', 'alice@email.com', 20, false, ['LS', 'BD', 'ED']];
+
+console.log(person[4]); //=> ['LS', 'BD', 'ED']
+console.log(person.at(-1)); //=> ['LS', 'BD', 'ED']
+console.log(person[4][0]); //=> 'LS'
+console.log(person.at(-1)[0]); //=> 'LS'
+
+// destructuring arrays
+// const name = person[0];
+// const email = person[1];
+
+// const [name, email] = ['Alice', 'alice@email.com', 20, false, ['LS', 'BD', 'ED']]
+// const [name, email] = person;
+// const [name, email, , , subjects] = person;
+const [name, email, , , [firstSubject]] = person;
+
+console.log(name); //=> 'Alice'
+
+// const state = useState('');
+// const [name, setName] = useState('');
+
+// spread operator
+const id = 123;
+
+const student = [id, ...person];
+
+console.log(student); //=> [123, 'Alice', 'alice@email.com', 20, false, ['LS', 'BD', 'ED']]
+
+console.log(Math.min(10, 20, 5)); //=> 5
+console.log(Math.min([10, 20, 5])); //=> NaN
+console.log(Math.min(...[10, 20, 5])); //=> 5
+
+// iteration
+// [10, 20, 30]
+values = [10, 20, 30];
+
+// for
+for (let index = 0; index < values.length; index++) {
+  console.log(values[index]);
+}
+
+// for..of [10, 20, 30]
+for (const value of values) {
+  console.log(value);
+}
+
+// for..in [0, 1, 2]
+for (const index in values) {
+  console.log(values[index]);
+}
+
+// entries
+for (const [index, value] of values.entries()) {
+  console.log(value);
+}
+
+// Object Array
+// Property: length
+// Mutator: push(), unshift(), pop(), shift(), reverse(), sort(), splice()
+// Accessor: includes(), join(), slice()
+// Iteration: map(), filter(), reduce(), every(), some()
--- classnotes/ecma/modules/esm-named-alias/main.js
+++ classnotes/ecma/modules/esm-named-alias/main.js
@@ -1,6 +1,17 @@
 import * as Calc from './lib.js';
+import {
+  sum as addition,
+  minus as subtraction,
+  multiply as multiplication,
+  divide as division,
+} from './lib.js';
 
 console.log(Calc.sum(1, 1)); //=> 2
 console.log(Calc.minus(1, 1)); //=> 0
 console.log(Calc.multiply(2, 1)); //=> 2
 console.log(Calc.divide(2, 1)); //=> 2
+
+console.log(addition(1, 1)); //=> 2
+console.log(subtraction(1, 1)); //=> 0
+console.log(multiplication(2, 1)); //=> 2
+console.log(division(2, 1)); //=> 2
