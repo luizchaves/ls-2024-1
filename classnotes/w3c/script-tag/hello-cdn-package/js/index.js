5d92d9aa6b68547b44945738d0985cd0f4c04b90
Luiz <lucachaves@gmail.com>
2024-03-27 18:41:03 -0300

    fix: import cpf-cnpj-validator

--- classnotes/w3c/script-tag/hello-cdn-package/js/index.js
+++ classnotes/w3c/script-tag/hello-cdn-package/js/index.js
@@ -1,7 +1,7 @@
 import { sum } from './lib/math.js';
-// import { derivative } from 'mathjs';
-// import { cpf } from 'cpf-cnpj-validator';
+// https://www.jsdelivr.com/package/npm/cpf-cnpj-validator
+import { cpf } from 'https://cdn.jsdelivr.net/npm/cpf-cnpj-validator@1.0.3/+esm';
 
 console.log(sum(1, 1));
 console.log(math.derivative('x^2', 'x').toString()); //=> 2 * x
-// console.log(cpf.generate());
+console.log(cpf.generate());
