602133bdb907da7b83400380a4944377755ca4e7
Luiz Carlos <lucachaves@gmail.com>
2024-07-01 16:12:28 -0300

    country map

--- exercises/web-script-country-flags/js/main.js
+++ exercises/web-script-country-flags/js/main.js
@@ -1,12 +1,18 @@
 import flags from './model/flags.js';
 
-const main = document.querySelector('main.row');
-
-for (const country of flags) {
-  const view = `<div class="flag col-2 my-2 text-center">
+function countryComponent(country) {
+  return `<div class="flag col-2 my-2 text-center">
     <img src="./imgs/flags/${country.id}.png" alt="${country.name}">
     <p>${country.name}</p>
   </div>`;
-
-  main.insertAdjacentHTML('beforeend', view);
 }
+
+const main = document.querySelector('main.row');
+
+// for (const country of flags) {
+//   const view = countryComponent(country);
+
+//   main.insertAdjacentHTML('beforeend', view);
+// }
+
+main.innerHTML = flags.map((country) => countryComponent(country)).join('');
